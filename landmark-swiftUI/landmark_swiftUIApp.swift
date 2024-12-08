61afe867255cfd338cd9cbb02170101d2059844e
Tim S <tsloncz@gmail.com>
2024-12-07 20:54:30 -0700

    Initial Commit

---
+++ landmark-swiftUI/landmark_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmark_swiftUIApp.swift
+//  landmark-swiftUI
+//
+//  Created by Tim Sloncz on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct landmark_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ landmark-swiftUITests/landmark_swiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  landmark_swiftUITests.swift
+//  landmark-swiftUITests
+//
+//  Created by Tim Sloncz on 12/7/24.
+//
+
+import Testing
+@testable import landmark_swiftUI
+
+struct landmark_swiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
